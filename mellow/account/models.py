37d38064087f0bb0ea8b405a23bb6c5f68b1521d
fredcode <fredcode23@gmail.com>
2022-09-29 03:52:20 +0800

    signup page added

--- mellow/account/models.py
+++ mellow/account/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class CustomUser(AbstractUser):
     Email_Address = models.EmailField(verbose_name="email", max_length=90, unique=True, blank=True, null=True, default=None)
     name = models.CharField(max_length=30, blank=True, null=True)
-
+    username = None
+    
     USERNAME_FIELD = 'Email_Address'
 
     def __str__(self):
