80a175ede27254bb01cf494353b8c896cbb7ebd9
fredcode <fredcode23@gmail.com>
2023-03-12 18:25:20 +0800

    edit notifications

--- mellow/mellow/settings.py
+++ mellow/mellow/settings.py
@@ -79,9 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mellow.wsgi.application'
 
-
-...
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
--- mellow/project/models.py
+++ mellow/project/models.py
@@ -43,11 +43,11 @@ class Comment(models.Model):
 
 
 class Notifications(models.Model):
-    message = models.CharField(max_length=255)
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sender')
-    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='recipient')
+    message = models.CharField(max_length=255, null=True,blank=True)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE,null=True,blank=True, related_name='sender')
+    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE,null=True,blank=True, related_name='recipient')
     timestamp = models.DateTimeField(auto_now_add=True)
-    read = models.BooleanField(default=False)
+    read = models.BooleanField(default=False, null=True,blank=True)
 
     def __str__(self):
         return self.message
